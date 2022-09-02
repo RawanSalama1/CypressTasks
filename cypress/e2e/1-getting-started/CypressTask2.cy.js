d3f2b1523e72c4d731a83ff8f3183491d3e32c7f
RawanSalama1 <rawan.salama@axsos.me>
2022-09-02 15:09:43 +0300

    Cypress Task2

---
+++ cypress/e2e/1-getting-started/CypressTask2.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+import data from '../../fixtures/keywords.json'
+describe('Fill the form using the data stored in data.json file.', () => {
+
+    beforeEach(() => {
+        cy.visit('https://demoqa.com/text-box')
+    })
+    it('Fill the form', () => {
+
+        cy.get("#userName").type(data.data1)
+        cy.get('#userEmail').type(data.data2)
+        cy.get('#currentAddress').type(data.data3)
+        cy.get('#permanentAddress').type(data.data4)
+        cy.get('#submit').click()
+    })
+
+})
