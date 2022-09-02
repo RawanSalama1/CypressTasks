82f3f7d03b759bdedf869783e7dee4296b78bc62
RawanSalama1 <rawan.salama@axsos.me>
2022-09-02 16:00:11 +0300

    Upload File

---
+++ cypress/e2e/1-getting-started/UploadTask.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+describe('Fill the form using the data stored in data.json file.', () => {
+
+    beforeEach(() => {
+        cy.visit('https://demoqa.com/upload-download')
+    })
+    it('Upload File Successfully', () => {
+
+        const imageFile = 'sunset.png'
+
+        cy.get('input[type = file]').selectFile('cypress/fixtures/sunset.png', { action: 'drag-drop' })
+        cy.get('#uploadedFilePath').should('exist')
+    })
+})
