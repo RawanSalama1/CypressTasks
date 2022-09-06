6bd2ce8fe08e69cff7f7d2792800a17fb2367f0f
RawanSalama1 <rawan.salama@axsos.me>
2022-09-06 14:49:49 +0300

    Check each index in the array and compare it

--- cypress/e2e/1-getting-started/downloadFile.cy.js
+++ cypress/e2e/1-getting-started/downloadFile.cy.js
@@ -1,29 +1,28 @@
 /// <reference types="cypress" />
+import test from "../../fixtures/data.json"
 const filePath = '/Users/rs2/Downloads/SampleCSVFile_2kb.csv'
-const data = [
-    "1",
-    "Eldon Base for stackable storage shelf, platinum",
-    "Muhammed MacIntyre",
-    "3",
-    "-213.25",
-    "38.94",
-    "35",
-    "Nunavut",
-    "Storage & Organization",
-    "0.8",
-]
 
-describe('downloaf csv file', () => {
+describe('download csv file', () => {
     it('Check rows in csv file', () => {
         cy.visit('https://sample-videos.com/download-sample-csv.php')
-        // cy.wait(1000)
         cy.get('.download_csv').eq(0).click
         cy.wait(2000);
-        // call the parseXlsx task we created above to parse the excel and return data as json
         cy.task('parseXlsx', { filePath }).then(
             jsonData => {
-                // finally we write the assertion rule to check if that data matches the data we expected the excel file to have.
-                expect(jsonData[0].data[0]).to.eqls(data);
+                expect(jsonData[0].data[0][0]).to.eq(test.data1)
+                expect(jsonData[0].data[0][1]).to.eq(test.data2)
+                expect(jsonData[0].data[0][2]).to.eq(test.data3)
+                expect(jsonData[0].data[0][3]).to.eq(test.data4)
+                expect(jsonData[0].data[0][4]).to.eq(test.data5)
+                expect(jsonData[0].data[0][5]).to.eq(test.data6)
+                expect(jsonData[0].data[0][6]).to.eq(test.data7)
+                expect(jsonData[0].data[0][7]).to.eq(test.data8)
+                expect(jsonData[0].data[0][8]).to.eq(test.data9)
+                expect(jsonData[0].data[0][9]).to.eq(test.data10)
+
+
+
+
             }
         );
     })
