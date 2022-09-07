65396cd113e9cfd6341b01ad74c3cf08837a92b8
RawanSalama1 <rawan.salama@axsos.me>
2022-09-07 10:16:18 +0300

    //Report Test Cases using Mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,27 +1,24 @@
 const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 const xlsx = require("node-xlsx").default;
 const fs = require("fs");
 const path = require("path");
 
-module.exports = {
-  ...(on, config) => {
-    // on("before:browser:launch", (browser = {}, launchOptions) => {
-    //   const downloadDirectory = path.join(__dirname, "..", "excelDownloads");
-    //   if (browser.family === "chromium") {
-    //     launchOptions.preferences.default["download"] = {
-    //       default_directory: downloadDirectory
-    //     };
-    //   }
-    //   return launchOptions;
-    // });
-    // `on` is used to hook into various events Cypress emits
-
-
-  },
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
 
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
       on("task", {
         parseXlsx({ filePath }) {
           return new Promise((resolve, reject) => {
@@ -40,5 +37,6 @@ module.exports = {
 
   },
 
-};
+});
+
 
